84ec60ac91716e2acd16a173dea8b93d2e71331e
Paul Moore <p.f.moore@gmail.com>
2021-03-21 09:20:15 +0000

    pystartup: Add a pretty version of dir() (idea from discourse)

--- config/.pystartup.py
+++ config/.pystartup.py
@@ -6,3 +6,8 @@ sys.path.append(os.path.join(os.path.dirname(__file__), "pyrepl-deps.zip"))
 
 from rich import print, pretty
 pretty.install()
+
+# See https://discuss.python.org/t/add-a-flag-hide-magic-names-in-dir/7276/2
+def ddir(obj):
+    return [a + '()' if callable(getattr(obj, a)) else a
+            for a in dir(obj) if not (a[:2] == '__' == a[-2:])]
